130ba86a8cd572457822ad7465476fb72f6a37d2
Fausto <fausto@group1245.com>
2026-04-22 10:53:37 -0300

    refactor(head): remove unnecessary whitespace and clean up meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Monitor de Preços - Encontre os melhores preços!')</title>
-
     <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/favicon/favicon-96x96.png') }}" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/images/favicon/favicon.svg') }}" />
     <link rel="shortcut icon" href="{{ Vite::asset('resources/images/favicon/favicon.ico') }}" />
     <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/images/favicon/apple-touch-icon.png') }}" />
     <link rel="manifest" href="{{ Vite::asset('resources/images/favicon/site.webmanifest') }}" />
-
     <meta name="description" content="@yield('description', 'Compare preços de produtos de lojas virtuais de todo o Brasil e encontre as melhores ofertas.')">
     @stack('meta')
     @vite('resources/css/app.css')
